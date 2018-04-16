3d11d52ad6270c654a4f7066b892e7b5b9f64e7d
drg91 <diegogarbiglia@gmail.com>
2018-04-16 14:54:01 -0300

    Update

--- openCV/stitching/CMakeFiles/stitching.dir/DependInfo.cmake
+++ openCV/stitching/CMakeFiles/stitching.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/drg91/Documents/example_cmake/stitching.cpp" "/Users/drg91/Documents/example_cmake/CMakeFiles/stitching.dir/stitching.cpp.o"
+  "/home/pi/Tesis/openCV/stitching/stitching.cpp" "/home/pi/Tesis/openCV/stitching/CMakeFiles/stitching.dir/stitching.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
